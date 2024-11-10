8ea52f912c345b17571d51757dcce48dc8b4fc15
HaramSs <haram8274@gmail.com>
2024-11-10 22:44:00 +0900

    front fin

--- src/main/java/com/manggee/blog/BlogController.java
+++ src/main/java/com/manggee/blog/BlogController.java
@@ -1,7 +1,10 @@
 package com.manggee.blog;
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlogController {
@@ -12,4 +15,5 @@ public class BlogController {
 
     @GetMapping("/register")
     public String join() {return "register";}
+
 }
